8e06006263843362e552f0c4006daa72425eb4ee
Nikhil Devasia <nikhildevasia2000@gmail.com>
2023-10-07 15:22:56 +0530

    faiss error

--- app.py
+++ app.py
@@ -1,96 +1,122 @@
 import streamlit as st
+from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
-# from langchain.embeddings import OpenAIEmbeddings
-from langchain.embeddings import HuggingFaceInstructEmbeddings
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
+from langchain.chat_models import ChatOpenAI
 from langchain.memory import ConversationBufferMemory
-from langchain.chains import conversational_retrieval
-# from langchain.chat_models import ChatOpenAI
+from langchain.chains import ConversationalRetrievalChain
 from htmlTemplate import css, bot_template, user_template
+from langchain.llms import HuggingFaceHub
 
 
 def get_pdf_text(pdf_docs):
     text = ""
     for pdf in pdf_docs:
         pdf_reader = PdfReader(pdf)
-        for pages in pdf_reader.pages:
-            text += pages.extract_text()
+        for page in pdf_reader.pages:
+            text += page.extract_text()
     return text
 
 
-def get_text_chunks(raw_text):
+def get_text_chunks(text):
     text_splitter = CharacterTextSplitter(
         separator="\n",
         chunk_size=1000,
         chunk_overlap=200,
         length_function=len
     )
-    chunks = text_splitter.split_text(raw_text)
+    chunks = text_splitter.split_text(text)
     return chunks
 
-def get_vectorstore(text_chunks):
-    embedding = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
-    vectorstore = FAISS.from_texts(text_chunks, embedding=embedding)
+
+def get_vectorstore(text_chunks, batch_size=10):
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="hkunlp/instructor-xl")
+    vectorstore = FAISS.from_texts.batched(
+        texts=text_chunks,
+        batch_size=batch_size,
+        embedding=embeddings,
+    )
     return vectorstore
 
-def get_conversation_chain(vectorstore):
-    llm = 
-    memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
-    conversation_chain = conversational_retrieval.from_llm(
-        llm = llm,
-        retriever = vectorstore.as_retriever(),
-        memory = memory
 
+def get_conversation_chain(vectorstore):
+    # llm = ChatOpenAI()
+    llm = HuggingFaceHub(repo_id="google/flan-t5-xxl", model_kwargs={"temperature":0.5, "max_length":512})
+
+    memory = ConversationBufferMemory(
+        memory_key='chat_history', return_messages=True)
+    conversation_chain = ConversationalRetrievalChain.from_llm(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
     )
     return conversation_chain
 
-def handle_userinput(user_question):
-    response = st.session_state.conversation({"question": user_question})
-    st.session_state.chat_history.append(response['chat_history'])
 
-    for i, message in enumerate(st.scatter_chart.chat_history):
-        if i % 2 == 0:
-            st.write(user_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
+def handle_userinput(user_question):
+    try:
+        response = st.session_state.conversation({'question': user_question})
+    except RuntimeError as e:
+        if "CUDA out of memory" in e.args[0]:
+            # Reduce the batch size and retry
+            st.session_state.conversation.retriever.batch_size //= 2
+            response = st.session_state.conversation({'question': user_question})
         else:
-            st.write(bot_template.replace("{{MSG}}", message['content']), unsafe_allow_html=True)
+            raise e
+
+    st.session_state.chat_history = response['chat_history']
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace(
+                "{{MSG}}", message.content), unsafe_allow_html=True)
+
 
 def main():
-    st.set_page_config(page_title="chat with multiple pdf", page_icon=":books")
+    load_dotenv()
+    st.set_page_config(page_title="Chat with multiple PDFs", page_icon=":books:")
     st.write(css, unsafe_allow_html=True)
 
     if "conversation" not in st.session_state:
         st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
 
-    st.header("chat with multiple pdf :books:")
-    user_question = st.text_input("Ask question about your documents:")
-
+    st.header("Chat with multiple PDFs :books:")
+    user_question = st.text_input("Ask a question about your documents:")
     if user_question:
         handle_userinput(user_question)
 
-    st.write(user_template.replace("{{MSG}}", "Hello robot"), unsafe_allow_html=True)
-    st.write(bot_template.replace("{{MSG}}", "Hello human"), unsafe_allow_html=True)
-
     with st.sidebar:
         st.subheader("Your documents")
-        pdf_docs = st.file_uploader("Upload your documents", accept_multiple_files=True)
+        pdf_docs = st.file_uploader("Upload your PDFs here and click on 'Process'", accept_multiple_files=True)
         if st.button("Process"):
             with st.spinner("Processing"):
-                # get the pdx 
+                # get pdf text
                 raw_text = get_pdf_text(pdf_docs)
 
-                # get the text chunks 
+                # get the text chunks
                 text_chunks = get_text_chunks(raw_text)
 
-                # create vector store 
-                vectorstore = get_vectorstore(text_chunks)
+                # create vector store in batches
+                batch_size = 10  # Adjust the batch size as needed
+                vectorstore = None
+                for i in range(0, len(text_chunks), batch_size):
+                    batch = text_chunks[i:i+batch_size]
+                    batch_vectorstore = get_vectorstore(batch)
+                    if vectorstore is None:
+                        vectorstore = batch_vectorstore
+                    else:
+                        vectorstore.concat(batch_vectorstore)
 
-                # create conversation
+                # create conversation chain
                 st.session_state.conversation = get_conversation_chain(vectorstore)
-    st.session_state.conversation
 
-
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
-
